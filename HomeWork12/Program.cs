01f39c791de0baaae18adc316874c6e8b712c2f6
Matveykylak <matveykylak@gmail.com>
2024-05-01 17:24:31 +0300

    SecondTask

--- HomeWork12/Program.cs
+++ HomeWork12/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-using HomeWork12.FirstTask;
+//using HomeWork12.FirstTask;
 
-var list = new List<string>() { "a", "a", "b", "b", "d", "f" , "g", "h"};
+//var list = new List<string>() { "a", "a", "b", "b", "d", "f" , "g", "h"};
 
 // WorkerWithLINQ.GetFirstOfString(list);
 // var res = WorkerWithLINQ.GetLastContainEE(list);
@@ -12,7 +12,7 @@ var list = new List<string>() { "a", "a", "b", "b", "d", "f" , "g", "h"};
 
 
 
-var res3 = WorkerWithLINQ.GetNumberOfUnique(list);
+//var res3 = WorkerWithLINQ.GetNumberOfUnique(list);
 
 
 
@@ -33,4 +33,47 @@ var res3 = WorkerWithLINQ.GetNumberOfUnique(list);
 //var res4 = WorkerWithLINQ.ReplaseDictionaryByList(d);
 
 //Console.WriteLine(string.Join(" ,", res4));
+
+
+
+//Second Task
+
+
+using HomeWork12.SecomdTask;
+
+User us = new User("Nda", "Xfsdf", "b");
+User us1 = new User("Nfg", "Dvcb", "a");
+User us2 = new User("Fgga", "Mc");
+User us3 = new User("Fgga", "Mb");
+User us4 = new User("Fgga", "Mv");
+User us5 = new User("Fgga", "Mv");
+User us6 = new User("Fgga", "M1");
+User us7 = new User("Fgga", "M2");
+User us8 = new User("Fgga", "M3");
+User us9 = new User("Fgga", "Mfgfg");
+User us10 = new User("Fgga", "Mfgfg");
+
+
+var users = new List<User>()
+{
+    us, us1, us2, us3, us4, us5, us6, us7, us8, us9, us10
+};
+
+
+
+/*var result = users.Select(u => u.LastName).ToList();
+users.OrderBy(u => u.LastName);
+result.Sort();*/
+
+//us.GetFullName(users);
+
+var result = us.SotredUsers(users);
+
+us.GetFullName(result);
+/*
+foreach (var user in result)
+{
+    Console.WriteLine(user);
+}
 Console.WriteLine("  ");
+*/
